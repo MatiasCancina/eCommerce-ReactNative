47fe732dcb0ee277a6411b47f86493dbc8c2591d
Matias Cancina <cancinamatias@gmail.com>
2024-07-16 23:28:02 -0300

    styles

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,23 +1,49 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Entypo } from "@expo/vector-icons";
+import { colors } from "../global/colors";
+import { capitalizeFirstLetter } from "../global/capitalizeFirstLetter";
 
 export default function CartItem({ item }) {
   return (
     <View style={styles.card}>
       <View style={styles.textContainer}>
         <Text style={styles.text}>
-          {item.title} X {item.quantity}
+          {capitalizeFirstLetter(item.title)}
         </Text>
         <Text>{item.brand}</Text>
-        <Text>$ {item.price}</Text>
+        <Text style={{ fontWeight: '300'}}>Quantity: {item.quantity}</Text>
+        <Text style={{ fontWeight: 'bold', fontSize: 16, color: colors.esmeraldGreen }}>$ {item.price}</Text>
       </View>
-      <Entypo name="trash" size={30} color={"black"} />
+      <View style={{ height: 100, width: 100, backgroundColor: 'red' }}></View>
+      <Entypo name="trash" size={30} color={"red"} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  card: {},
-  textContainer: {},
-  text: {},
+  card: {
+    height: 150,
+    backgroundColor: 'white',
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textContainer: {
+    width: "50%",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
+  text: {
+    fontSize: 19,
+    fontWeight: '500',
+  },
+  text2: {
+    fontSize: 14,
+    color: colors.green900,
+  },
+
 });
