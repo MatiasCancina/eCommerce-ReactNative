f2d6e9b3f0fae2fef03ba6dd832c7cb2560122cf
Matias Cancina <cancinamatias@gmail.com>
2024-07-20 16:15:52 -0300

    delete info{}

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,14 +2,8 @@ import { Button, StyleSheet, View } from "react-native";
 import Categories from "../components/Categories";
 
 export default function Home({ navigation }) {
-  const info = {
-    id: 1243,
-    total: 300,
-    medidas: { peso: 45, alto: 453 },
-  };
-
   const goUs = () => {
-    navigation.navigate("Us", info);
+    navigation.navigate("Us");
   };
 
   return (
